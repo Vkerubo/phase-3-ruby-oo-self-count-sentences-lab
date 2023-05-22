7b22df2ee3057bddc13e3244606d7c07ab03dd2d
Valentine <kerubovalentine1@gmail.com>
2023-05-22 22:20:28 +0300

    complete assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    pp self.end_with?(".")
   end
 
   def question?
-
+    pp self.end_with?("?")
   end
 
   def exclamation?
-
+    pp self.end_with?("!")
   end
 
   def count_sentences
-
+    self.nil? || self.empty? ? 0: self.split(/[.?!]+/).count
   end
 end
